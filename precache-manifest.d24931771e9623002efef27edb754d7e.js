9a3a4090376fe21a34fce81cc3ddce27c1484f59
Sandunga <sandra.cuevastorres@gmail.com>
2019-05-10 13:10:32 -0500

    Updates

---
+++ precache-manifest.d24931771e9623002efef27edb754d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1420f28763339bb9c6ebdb9d15ca2e",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "4459de76cb9fd0e93153",
+    "url": "/GDL002-burger-queen/static/css/main.9b5e1a66.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GDL002-burger-queen/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4459de76cb9fd0e93153",
+    "url": "/GDL002-burger-queen/static/js/main.6f2174cf.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GDL002-burger-queen/static/media/logo.5d5d9eef.svg"
+  }
+]);
