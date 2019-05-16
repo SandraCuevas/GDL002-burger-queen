3beaa708308df63b3778c135782d5fe0a2304177
Sandunga <sandra.cuevastorres@gmail.com>
2019-05-16 16:33:38 -0500

    constructor y super

--- burguer_queen/src/components/menu.js
+++ burguer_queen/src/components/menu.js
@@ -1,30 +1,73 @@
-import React from 'react';
-import { menu } from '../menu.json'
+import React, {Component} from 'react';
+
+import  {menu}  from '../menu.json'
+
+class ShowMenu extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+           orders:[]
+        }
+         this.submit = this.submit.bind(this);
+}
+
+submit(nameItem, price){
+    //console.log(nameItem, price)
+    //console.log('props', this.props.addOrders)
+     const orders = this.state.orders;
+ 
+     const order = {
+       item: nameItem,
+       price: price
+     }
+ 
+     orders.push(order);
+     this.props.addOrders(this.state.orders);
+     }
+ 
 
 
 
-const showMenu = (props) => (
+     render(){
+
+        return(
    
-    <div className="main-content">
-        <div className="container">
-            <h2> MENUSSSS </h2>
-            <div class="card">
-                <div class="card-header">
-                    MENU
-                </div>
-                {menu.map((menuDetail, index)=>
-                    <ul class="list-group list-group-flush" key={index}>
-                        <button class="list-group-item">
-                            <h5 className="card-title">{menuDetail.Item}</h5>
-                            <p className="card-text">{menuDetail.Price}</p>
-                        </button>
-                    </ul>
-                    )
-                }
-            </div>
-        </div>
+            <div className="main-content">
+                <div className="container">
+                    <h2> MENUS </h2>
+                        <div class="row">
+                            <div className="col">
+                                MENU
+                        </div>
+                        <div className="col">
+                                ORDER
+                        </div>
+                        <div className="w-100">
+                            <div className="col-md-6">
+                            
+                                {menu.map((menuDetail, index)=>
+                                    <div className="list-group list-group-flush col mt-4"  key={index}>
+                                        <button className="list-group-item">
+                                            <h5 className="card-title">{menuDetail.item}</h5>
+                                            <p className="card-text">{menuDetail.price}</p>
+                                        </button>
+                                    </div>
+                                    )
+                                }
+                            </div>
+                            <div className="col"></div>
+                            <div className="w-100"></div>
+                        </div>
+                    </div>
         
-    </div>
-);
 
-export default showMenu;
+                </div>
+                
+            </div>
+        );
+    
+        }
+        
+    }
+
+    export default ShowMenu;
